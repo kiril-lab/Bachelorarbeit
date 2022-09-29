59538561f7380062a6ed486f0ab0d30c6d7f5fbc
Kiril <kiril_s@yahoo.com>
2022-09-29 14:59:35 +0200

    change struktur

--- src/components/Statistik2.tsx
+++ src/components/Statistik2.tsx
@@ -46,8 +46,10 @@ function Statistik1({ data1, data2, data3 }: Props) {
       const month = new Date(i * 1000).getMonth() + 1;
       return { month: month, year: year };
     });
-    const year = date.filter((e) => e.year == years);
-    const month = date.filter((e) => e.month == months && e.year == years);
+    const year = date.filter((e) => e.year == years).length;
+    const month = date.filter(
+      (e) => e.month == months && e.year == years
+    ).length;
     return { date, year, month };
   };
   useEffect(() => {
@@ -83,165 +85,165 @@ function Statistik1({ data1, data2, data3 }: Props) {
     });
     setProposalYear({
       year_one:
-        proposals_date(data1, Year.one).year.length +
-        proposals_date(data2, Year.one).year.length +
-        proposals_date(data3, Year.one).year.length,
+        proposals_date(data1, Year.one).year +
+        proposals_date(data2, Year.one).year +
+        proposals_date(data3, Year.one).year,
       year_two:
-        proposals_date(data1, Year.two).year.length +
-        proposals_date(data2, Year.two).year.length +
-        proposals_date(data3, Year.two).year.length,
+        proposals_date(data1, Year.two).year +
+        proposals_date(data2, Year.two).year +
+        proposals_date(data3, Year.two).year,
       year_three:
-        proposals_date(data1, Year.three).year.length +
-        proposals_date(data2, Year.three).year.length +
-        proposals_date(data3, Year.three).year.length,
+        proposals_date(data1, Year.three).year +
+        proposals_date(data2, Year.three).year +
+        proposals_date(data3, Year.three).year,
     });
     setProposalMonth({
       month_one_year_one:
-        proposals_date(data1, Year.one, Month.one).month.length +
-        proposals_date(data2, Year.one, Month.one).month.length +
-        proposals_date(data3, Year.one, Month.one).month.length,
+        proposals_date(data1, Year.one, Month.one).month +
+        proposals_date(data2, Year.one, Month.one).month +
+        proposals_date(data3, Year.one, Month.one).month,
       month_two_year_one:
-        proposals_date(data1, Year.one, Month.two).month.length +
-        proposals_date(data2, Year.one, Month.two).month.length +
-        proposals_date(data3, Year.one, Month.two).month.length,
+        proposals_date(data1, Year.one, Month.two).month +
+        proposals_date(data2, Year.one, Month.two).month +
+        proposals_date(data3, Year.one, Month.two).month,
       month_three_year_one:
-        proposals_date(data1, Year.one, Month.three).month.length +
-        proposals_date(data2, Year.one, Month.three).month.length +
-        proposals_date(data3, Year.one, Month.three).month.length,
+        proposals_date(data1, Year.one, Month.three).month +
+        proposals_date(data2, Year.one, Month.three).month +
+        proposals_date(data3, Year.one, Month.three).month,
       month_four_year_one:
-        proposals_date(data1, Year.one, Month.four).month.length +
-        proposals_date(data2, Year.one, Month.four).month.length +
-        proposals_date(data3, Year.one, Month.four).month.length,
+        proposals_date(data1, Year.one, Month.four).month +
+        proposals_date(data2, Year.one, Month.four).month +
+        proposals_date(data3, Year.one, Month.four).month,
       month_five_year_one:
-        proposals_date(data1, Year.one, Month.five).month.length +
-        proposals_date(data2, Year.one, Month.five).month.length +
-        proposals_date(data3, Year.one, Month.five).month.length,
+        proposals_date(data1, Year.one, Month.five).month +
+        proposals_date(data2, Year.one, Month.five).month +
+        proposals_date(data3, Year.one, Month.five).month,
       month_six_year_one:
-        proposals_date(data1, Year.one, Month.six).month.length +
-        proposals_date(data2, Year.one, Month.six).month.length +
-        proposals_date(data3, Year.one, Month.six).month.length,
+        proposals_date(data1, Year.one, Month.six).month +
+        proposals_date(data2, Year.one, Month.six).month +
+        proposals_date(data3, Year.one, Month.six).month,
       month_seven_year_one:
-        proposals_date(data1, Year.one, Month.seven).month.length +
-        proposals_date(data2, Year.one, Month.seven).month.length +
-        proposals_date(data3, Year.one, Month.seven).month.length,
+        proposals_date(data1, Year.one, Month.seven).month +
+        proposals_date(data2, Year.one, Month.seven).month +
+        proposals_date(data3, Year.one, Month.seven).month,
       month_eight_year_one:
-        proposals_date(data1, Year.one, Month.eight).month.length +
-        proposals_date(data2, Year.one, Month.eight).month.length +
-        proposals_date(data3, Year.one, Month.eight).month.length,
+        proposals_date(data1, Year.one, Month.eight).month +
+        proposals_date(data2, Year.one, Month.eight).month +
+        proposals_date(data3, Year.one, Month.eight).month,
       month_nine_year_one:
-        proposals_date(data1, Year.one, Month.nine).month.length +
-        proposals_date(data2, Year.one, Month.nine).month.length +
-        proposals_date(data3, Year.one, Month.nine).month.length,
+        proposals_date(data1, Year.one, Month.nine).month +
+        proposals_date(data2, Year.one, Month.nine).month +
+        proposals_date(data3, Year.one, Month.nine).month,
       month_ten_year_one:
-        proposals_date(data1, Year.one, Month.ten).month.length +
-        proposals_date(data2, Year.one, Month.ten).month.length +
-        proposals_date(data3, Year.one, Month.ten).month.length,
+        proposals_date(data1, Year.one, Month.ten).month +
+        proposals_date(data2, Year.one, Month.ten).month +
+        proposals_date(data3, Year.one, Month.ten).month,
       month_eleven_year_one:
-        proposals_date(data1, Year.one, Month.eleven).month.length +
-        proposals_date(data2, Year.one, Month.eleven).month.length +
-        proposals_date(data3, Year.one, Month.eleven).month.length,
+        proposals_date(data1, Year.one, Month.eleven).month +
+        proposals_date(data2, Year.one, Month.eleven).month +
+        proposals_date(data3, Year.one, Month.eleven).month,
       month_twelve_year_one:
-        proposals_date(data1, Year.one, Month.twelve).month.length +
-        proposals_date(data2, Year.one, Month.twelve).month.length +
-        proposals_date(data3, Year.one, Month.twelve).month.length,
+        proposals_date(data1, Year.one, Month.twelve).month +
+        proposals_date(data2, Year.one, Month.twelve).month +
+        proposals_date(data3, Year.one, Month.twelve).month,
       month_one_year_two:
-        proposals_date(data1, Year.two, Month.one).month.length +
-        proposals_date(data2, Year.two, Month.one).month.length +
-        proposals_date(data3, Year.two, Month.one).month.length,
+        proposals_date(data1, Year.two, Month.one).month +
+        proposals_date(data2, Year.two, Month.one).month +
+        proposals_date(data3, Year.two, Month.one).month,
       month_two_year_two:
-        proposals_date(data1, Year.two, Month.two).month.length +
-        proposals_date(data2, Year.two, Month.two).month.length +
-        proposals_date(data3, Year.two, Month.two).month.length,
+        proposals_date(data1, Year.two, Month.two).month +
+        proposals_date(data2, Year.two, Month.two).month +
+        proposals_date(data3, Year.two, Month.two).month,
       month_three_year_two:
-        proposals_date(data1, Year.two, Month.three).month.length +
-        proposals_date(data2, Year.two, Month.three).month.length +
-        proposals_date(data3, Year.two, Month.three).month.length,
+        proposals_date(data1, Year.two, Month.three).month +
+        proposals_date(data2, Year.two, Month.three).month +
+        proposals_date(data3, Year.two, Month.three).month,
       month_four_year_two:
-        proposals_date(data1, Year.two, Month.four).month.length +
-        proposals_date(data2, Year.two, Month.four).month.length +
-        proposals_date(data3, Year.two, Month.four).month.length,
+        proposals_date(data1, Year.two, Month.four).month +
+        proposals_date(data2, Year.two, Month.four).month +
+        proposals_date(data3, Year.two, Month.four).month,
       month_five_year_two:
-        proposals_date(data1, Year.two, Month.five).month.length +
-        proposals_date(data2, Year.two, Month.five).month.length +
-        proposals_date(data3, Year.two, Month.five).month.length,
+        proposals_date(data1, Year.two, Month.five).month +
+        proposals_date(data2, Year.two, Month.five).month +
+        proposals_date(data3, Year.two, Month.five).month,
       month_six_year_two:
-        proposals_date(data1, Year.two, Month.six).month.length +
-        proposals_date(data2, Year.two, Month.six).month.length +
-        proposals_date(data3, Year.two, Month.six).month.length,
+        proposals_date(data1, Year.two, Month.six).month +
+        proposals_date(data2, Year.two, Month.six).month +
+        proposals_date(data3, Year.two, Month.six).month,
       month_seven_year_two:
-        proposals_date(data1, Year.two, Month.seven).month.length +
-        proposals_date(data2, Year.two, Month.seven).month.length +
-        proposals_date(data3, Year.two, Month.seven).month.length,
+        proposals_date(data1, Year.two, Month.seven).month +
+        proposals_date(data2, Year.two, Month.seven).month +
+        proposals_date(data3, Year.two, Month.seven).month,
       month_eight_year_two:
-        proposals_date(data1, Year.two, Month.eight).month.length +
-        proposals_date(data2, Year.two, Month.eight).month.length +
-        proposals_date(data3, Year.two, Month.eight).month.length,
+        proposals_date(data1, Year.two, Month.eight).month +
+        proposals_date(data2, Year.two, Month.eight).month +
+        proposals_date(data3, Year.two, Month.eight).month,
       month_nine_year_two:
-        proposals_date(data1, Year.two, Month.nine).month.length +
-        proposals_date(data2, Year.two, Month.nine).month.length +
-        proposals_date(data3, Year.two, Month.nine).month.length,
+        proposals_date(data1, Year.two, Month.nine).month +
+        proposals_date(data2, Year.two, Month.nine).month +
+        proposals_date(data3, Year.two, Month.nine).month,
       month_ten_year_two:
-        proposals_date(data1, Year.two, Month.ten).month.length +
-        proposals_date(data2, Year.two, Month.ten).month.length +
-        proposals_date(data3, Year.two, Month.ten).month.length,
+        proposals_date(data1, Year.two, Month.ten).month +
+        proposals_date(data2, Year.two, Month.ten).month +
+        proposals_date(data3, Year.two, Month.ten).month,
       month_eleven_year_two:
-        proposals_date(data1, Year.two, Month.eleven).month.length +
-        proposals_date(data2, Year.two, Month.eleven).month.length +
-        proposals_date(data3, Year.two, Month.eleven).month.length,
+        proposals_date(data1, Year.two, Month.eleven).month +
+        proposals_date(data2, Year.two, Month.eleven).month +
+        proposals_date(data3, Year.two, Month.eleven).month,
       month_twelve_year_two:
-        proposals_date(data1, Year.two, Month.twelve).month.length +
-        proposals_date(data2, Year.two, Month.twelve).month.length +
-        proposals_date(data3, Year.two, Month.twelve).month.length,
+        proposals_date(data1, Year.two, Month.twelve).month +
+        proposals_date(data2, Year.two, Month.twelve).month +
+        proposals_date(data3, Year.two, Month.twelve).month,
       month_one_year_three:
-        proposals_date(data1, Year.three, Month.one).month.length +
-        proposals_date(data2, Year.three, Month.one).month.length +
-        proposals_date(data3, Year.three, Month.one).month.length,
+        proposals_date(data1, Year.three, Month.one).month +
+        proposals_date(data2, Year.three, Month.one).month +
+        proposals_date(data3, Year.three, Month.one).month,
       month_two_year_three:
-        proposals_date(data1, Year.three, Month.two).month.length +
-        proposals_date(data2, Year.three, Month.two).month.length +
-        proposals_date(data3, Year.three, Month.two).month.length,
+        proposals_date(data1, Year.three, Month.two).month +
+        proposals_date(data2, Year.three, Month.two).month +
+        proposals_date(data3, Year.three, Month.two).month,
       month_three_year_three:
-        proposals_date(data1, Year.three, Month.three).month.length +
-        proposals_date(data2, Year.three, Month.three).month.length +
-        proposals_date(data3, Year.three, Month.three).month.length,
+        proposals_date(data1, Year.three, Month.three).month +
+        proposals_date(data2, Year.three, Month.three).month +
+        proposals_date(data3, Year.three, Month.three).month,
       month_four_year_three:
-        proposals_date(data1, Year.three, Month.four).month.length +
-        proposals_date(data2, Year.three, Month.four).month.length +
-        proposals_date(data3, Year.three, Month.four).month.length,
+        proposals_date(data1, Year.three, Month.four).month +
+        proposals_date(data2, Year.three, Month.four).month +
+        proposals_date(data3, Year.three, Month.four).month,
       month_five_year_three:
-        proposals_date(data1, Year.three, Month.five).month.length +
-        proposals_date(data2, Year.three, Month.five).month.length +
-        proposals_date(data3, Year.three, Month.five).month.length,
+        proposals_date(data1, Year.three, Month.five).month +
+        proposals_date(data2, Year.three, Month.five).month +
+        proposals_date(data3, Year.three, Month.five).month,
       month_six_year_three:
-        proposals_date(data1, Year.three, Month.six).month.length +
-        proposals_date(data2, Year.three, Month.six).month.length +
-        proposals_date(data3, Year.three, Month.six).month.length,
+        proposals_date(data1, Year.three, Month.six).month +
+        proposals_date(data2, Year.three, Month.six).month +
+        proposals_date(data3, Year.three, Month.six).month,
       month_seven_year_three:
-        proposals_date(data1, Year.three, Month.seven).month.length +
-        proposals_date(data2, Year.three, Month.seven).month.length +
-        proposals_date(data3, Year.three, Month.seven).month.length,
+        proposals_date(data1, Year.three, Month.seven).month +
+        proposals_date(data2, Year.three, Month.seven).month +
+        proposals_date(data3, Year.three, Month.seven).month,
       month_eight_year_three:
-        proposals_date(data1, Year.three, Month.eight).month.length +
-        proposals_date(data2, Year.three, Month.eight).month.length +
-        proposals_date(data3, Year.three, Month.eight).month.length,
+        proposals_date(data1, Year.three, Month.eight).month +
+        proposals_date(data2, Year.three, Month.eight).month +
+        proposals_date(data3, Year.three, Month.eight).month,
       month_nine_year_three:
-        proposals_date(data1, Year.three, Month.nine).month.length +
-        proposals_date(data2, Year.three, Month.nine).month.length +
-        proposals_date(data3, Year.three, Month.nine).month.length,
+        proposals_date(data1, Year.three, Month.nine).month +
+        proposals_date(data2, Year.three, Month.nine).month +
+        proposals_date(data3, Year.three, Month.nine).month,
       month_ten_year_three:
-        proposals_date(data1, Year.three, Month.ten).month.length +
-        proposals_date(data2, Year.three, Month.ten).month.length +
-        proposals_date(data3, Year.three, Month.ten).month.length,
+        proposals_date(data1, Year.three, Month.ten).month +
+        proposals_date(data2, Year.three, Month.ten).month +
+        proposals_date(data3, Year.three, Month.ten).month,
       month_eleven_year_three:
-        proposals_date(data1, Year.three, Month.eleven).month.length +
-        proposals_date(data2, Year.three, Month.eleven).month.length +
-        proposals_date(data3, Year.three, Month.eleven).month.length,
+        proposals_date(data1, Year.three, Month.eleven).month +
+        proposals_date(data2, Year.three, Month.eleven).month +
+        proposals_date(data3, Year.three, Month.eleven).month,
       month_twelve_year_three:
-        proposals_date(data1, Year.three, Month.twelve).month.length +
-        proposals_date(data2, Year.three, Month.twelve).month.length +
-        proposals_date(data3, Year.three, Month.twelve).month.length,
+        proposals_date(data1, Year.three, Month.twelve).month +
+        proposals_date(data2, Year.three, Month.twelve).month +
+        proposals_date(data3, Year.three, Month.twelve).month,
     });
-  }, [data1]);
+  }, [data1,data2,data3]);
   return (
     <>
       {data1 && data2 && data3 ? (
